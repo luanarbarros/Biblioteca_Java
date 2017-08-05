45c3f262b577b8638f62063b32335244db9d890f
GabrielDaltro <gabriel.d.8@hotmail.com>
2017-08-05 18:59:08 -0300

    classe livro quase completa

--- modelo/Livro.java
+++ modelo/Livro.java
@@ -1,6 +1,44 @@
+package modelo;
+
 public class Livro {
 	
 	private String titulo;
 	private int quantidade;
+	private ArrayList<Autor> autores = new Array
+	
+	
+	public Livro(String t, int q)
+	{
+		titulo = t;
+		quantidade = q;
+	}
+	
+	public String getTitulo ()
+	{
+		return titulo;
+	}
+	
+	public void setTitulo (String t)
+	{
+		titulo = t;
+	}
+	
+	public int getQuantidade()
+	{
+		return quantidade;
+	}
+	
+	public void setQuantidade (int q)
+	{
+		quantidade = q;
+	}
+	
+	public String toString()
+	{
+		String text;
+		text = "Titulo= " + titulo + "quantidade" + quantidade;
+		return text;
+	}
+	
 
 }
